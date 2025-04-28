c25e46ea74c199eda0f04802bd7856bb70b3a76c
mikevidotto <mikevidotto@live.com>
2025-04-28 13:58:52 -0400

    imported ff

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mikevidotto/gmk
 
 go 1.24.1
+
+require github.com/mikevidotto/ff v0.0.0-20250428173715-f4b020d8edac // indirect
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
-package github.com/mikevidotto/gmk 
+package main
 
 import (
+	"github.com/mikevidotto/ff/ff"
 	"log"
-    "github.com/mikevidotto/ff"
 )
 
 func main() {
 	body := []byte("package main\n\nimport (\n  \"fmt\"\n)\n\nfunc main() {\n   fmt.Println(\"hello world\")\n}")
-    if err := ff.WriteIfNotExist("main.go", body); err != nil {
-        log.Fatal(err)
-    }
+	if err := ff.WriteIfNotExist("main.go", body); err != nil {
+		log.Fatal(err)
+	}
+    if err := ff.OpenInEditor("nvim", "./main.go"); err != nil {
+		log.Fatal(err)
+	}
 }
